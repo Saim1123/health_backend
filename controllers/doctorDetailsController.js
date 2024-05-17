da1928df9334600fe62dbcdab6e9f55dc5f8527e
Starla Patras <starlapatras@gmail.com>
2024-05-17 15:33:43 +0500

    add doctor details api

--- controllers/doctorDetailsController.js
+++ controllers/doctorDetailsController.js
@@ -1,5 +1,32 @@
+import { DoctorDetails } from "../models/doctorDetailsScehma.js";
+
 export const doctorDetails = async (req, res, next) => {
-  res.json({
-    message: "hi from server",
-  });
+  const {
+    first_name,
+    last_name,
+    education,
+    speciality,
+    phone_number,
+    city,
+    cnic_number,
+    gender,
+  } = req.body;
+
+  try {
+    const doctorDetails = new DoctorDetails({
+      first_name,
+      last_name,
+      education,
+      speciality,
+      phone_number,
+      city,
+      cnic_number,
+      gender,
+    });
+
+    await doctorDetails.save();
+    res.status(201).json({ message: "doctor registered", doctorDetails });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
--- models/doctorDetailsScehma.js
+++ models/doctorDetailsScehma.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
-import { captureRejectionSymbol } from "nodemailer/lib/xoauth2";
+// import { captureRejectionSymbol } from "nodemailer/lib/xoauth2";
 
 const doctorDetailsSchema = new mongoose.Schema({
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
   education: { type: String, required: true },
   speciality: { type: String, required: true },
-  phoneNumber: { type: Number, required: true },
+  phone_number: { type: Number, required: true },
   city: { type: String, required: true },
-  cnicNumber: { type: Number, required: true, maxLength: 13 },
+  cnic_number: { type: Number, required: true, maxLength: 13 },
   gender: { type: String, enum: ["male", "female", "other"], required: true },
   //   pmcCertificate: { type: string, required: true, maxLength: 13 },
 });
