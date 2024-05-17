e1150fea483264914dbcbf30cef4fc72a4fd68ff
Starla Patras <starlapatras@gmail.com>
2024-05-17 16:18:06 +0500

    add error handling

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import HttpError from "../models/Http-error.js";
 import { Users } from "../models/userSchema.js";
 import {
   signupValidation,
@@ -14,15 +15,16 @@ export const signup = async (req, res, next) => {
   const { name, email, password } = req.body;
   try {
     const existingUser = await Users.findOne({ email });
-    if (existingUser)
-      return res.status(400).json({ message: "User already exists." });
+    if (existingUser) {
+      return next(new HttpError(" User already exist", 500));
+    }
 
     const users = new Users({ name, email, password });
     await users.save();
 
     res.status(201).json({ message: "User registered ", users });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    return next(new HttpError("Signning up failed, try again", 500));
   }
 };
 
@@ -35,12 +37,13 @@ export const login = async (req, res, next) => {
   try {
     const users = await Users.findOne({ email });
     if (!users) {
-      return res.status(400).json({ message: "Invalid email or password" });
+      // return res.status(400).json({ message: "Invalid email or password" });
+      return next(new HttpError("Invalid email or password", 400));
     }
 
     const isMatch = await users.comparePassword(password);
     if (!isMatch) {
-      return res.status(400).json({ message: "Invalid email or password" });
+      return next(new HttpError("Invalid email or password", 400));
     }
 
     const token = jwt.sign({ id: users._id }, process.env.JWT_SECRET, {
@@ -49,6 +52,7 @@ export const login = async (req, res, next) => {
 
     res.json({ token });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    // res.status(500).json({ message: err.message });
+    return next(new HttpError("Loggig up failed , try gain ", 500));
   }
 };
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectDB from "./config/db.js";
 import { router as userRoutes } from "./routes/userRoutes.js";
 import { router as doctorDetailsRoute } from "./routes/doctorDetailsRoute.js";
+import HttpError from "./models/Http-error.js";
 
 const app = express();
 
@@ -11,6 +12,19 @@ connectDB();
 
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", doctorDetailsRoute);
+
+app.use((req, res, next) => {
+  const error = new HttpError("This route is not found", 404);
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "unknown error occured" });
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
