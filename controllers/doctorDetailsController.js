09bc0ff61d74ff46390fda463e65a049c62f8949
SaimTahir <127545146+Saim1123@users.noreply.github.com>
2024-05-22 15:58:48 +0500

    imported for validation the user_id.

--- controllers/doctorDetailsController.js
+++ controllers/doctorDetailsController.js
@@ -1,4 +1,5 @@
 import { DoctorDetails } from "../models/doctorDetailsScehma.js";
+import { Users } from "../models/userSchema.js";
 
 export const doctorDetails = async (req, res, next) => {
   const {
@@ -11,9 +12,13 @@ export const doctorDetails = async (req, res, next) => {
     cnic_number,
     gender,
   } = req.body;
-  const pmcCertificate = req.file.path;
 
   try {
+    const pmcCertificate = req.file.path;
+
+    const user = await Users.findById(doctor_id);
+    if (!user) res.status(400).send("user not found");
+
     const doctorDetails = new DoctorDetails({
       doctor_id,
       first_name,
